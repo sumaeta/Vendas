f727189960c354d0bf06a2afec572b832dbc9cbc
Sumaeta <brenosumaeta33@gmail.com>
2021-04-20 17:37:58 -0400

    Relacionamentos muitos-para-muitos

--- src/main/java/com/test/app/AppApplication.java
+++ src/main/java/com/test/app/AppApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.test.app.domain.Categoria;
+import com.test.app.domain.Produto;
 import com.test.app.repositories.CategoriaRepository;
+import com.test.app.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class AppApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
@@ -25,7 +30,20 @@ public class AppApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+				
+				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 
